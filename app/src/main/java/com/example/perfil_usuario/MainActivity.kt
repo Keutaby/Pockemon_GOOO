73e6aaa1435e61ec9543937840415aa1c17317f0
Keutaby <al217212@alumnos.uacj.mx>
2025-05-14 08:57:41 -0600

    [AGREGAR] imagenes para la mapa

--- app/src/main/java/com/example/perfil_usuario/MainActivity.kt
+++ app/src/main/java/com/example/perfil_usuario/MainActivity.kt
@@ -1,47 +1,91 @@
 package com.example.perfil_usuario
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.example.Perfil_Usuario.ControladoresMapa.GPSControlador
+import com.example.Perfil_Usuario.ControladoresMapa.usuario_GPS
+import com.example.Perfil_Usuario.PantallasMenu.MapaPokemones
+import org.osmdroid.config.Configuration
+import android.Manifest
+import androidx.compose.runtime.getValue
 import com.example.perfil_usuario.ui.theme.Perfil_UsuarioTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var gps_sensor: usuario_GPS
+    private val controlador_gps: GPSControlador by viewModels()
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+            gps_sensor = usuario_GPS(this)
+
+            if(!consultar_permisos_gps()){
+                requestPermissions(arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), permisos_totales_gps)
+            }
+            else{
+                controlador_gps.actualizar_permisos(true)
+            }
+
+            enableEdgeToEdge()
+            setContent {
+                val tenemos_permitido_usar_gps: Boolean by controlador_gps.autorizacion.observeAsState(initial = false)
+
+                LaunchedEffect(tenemos_permitido_usar_gps){
+                    if(tenemos_permitido_usar_gps){
+                        gps_sensor.configurar_actualizar_ubicacion(controlador_gps)
+                    }
+                }
+
+
         enableEdgeToEdge()
         setContent {
             Perfil_UsuarioTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
+                    MapaPokemones(controlador_gps)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+        val contexto = applicationContext
+        Configuration.getInstance().load(contexto, PreferenceManager.getDefaultSharedPreferences(contexto)) //download/import the osmDroid so it works, not android
+        Configuration.getInstance().userAgentValue = "Mapa"
+    }
+    private fun consultar_permisos_gps(): Boolean{
+        var permiso_fine_location = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        var permiso_coarse_location = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        return permiso_fine_location || permiso_coarse_location
+    }
+
+    companion object{
+        const val permisos_totales_gps = 123 //request code #
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Perfil_UsuarioTheme {
-        Greeting("Android")
+        MapaPokemones(controlador_gps = GPSControlador())
     }
 }
--- app/src/main/java/com/example/perfil_usuario/PantallasMenu/pantalla_mapa.kt
+++ app/src/main/java/com/example/perfil_usuario/PantallasMenu/pantalla_mapa.kt
@@ -1,6 +1,5 @@
 package com.example.Perfil_Usuario.PantallasMenu
 
-import android.content.res.Resources
 import android.graphics.drawable.Drawable
 import android.location.Location
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,12 +8,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
 import com.example.Perfil_Usuario.ControladoresMapa.GPSControlador
-import com.example.perfil_usuario.R
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
@@ -40,9 +36,12 @@ fun MapaPokemones(controlador_gps: GPSControlador){
         }){ mapa_view ->
 
         val marcador_ubicacion_actual = Marker(mapa_view)
+
         if(ubicacion_actual != null){
             marcador_ubicacion_actual.position = GeoPoint(ubicacion_actual!!) //tipo optional
             marcador_ubicacion_actual.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            val poke_gym_marker: Drawable? = ContextCompat.getDrawable(contexto, com.example.perfil_usuario.R.drawable.threed_gym)
+            marcador_ubicacion_actual.icon = poke_gym_marker
 
             mapa_view.controller.animateTo(marcador_ubicacion_actual.position) //to be shown a certain point
         }
@@ -50,16 +49,18 @@ fun MapaPokemones(controlador_gps: GPSControlador){
         val marcador_2 = Marker(mapa_view)
         val parque_veterans = GeoPoint(31.92019790477828, -106.42046109544629) //get from google maps, veterans pool
         marcador_2.position = parque_veterans
-        marcador_2.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        marcador_2.icon
-        val d: Drawable? = ContextCompat.getDrawable(contexto, R.drawable.ic_launcher_foreground)
-        marcador_2.icon = d
+        //marcador_2.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        //marcador_2.icon
+        val poke_gym_marker: Drawable? = ContextCompat.getDrawable(contexto, com.example.perfil_usuario.R.drawable.threed_gym)
+        marcador_2.icon = poke_gym_marker
 
         val marcador_3 = Marker(mapa_view)
         val parque_Eastside = GeoPoint(31.816170773485606, -106.53212165580622) //get from google maps, Westside Natatorium pool
         marcador_3.position = parque_Eastside
         marcador_3.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        marcador_3.icon
+        val pokestop_marker: Drawable? = ContextCompat.getDrawable(contexto, com.example.perfil_usuario.R.drawable.threed_pokestop)
+        marcador_3.icon = pokestop_marker
+
 
         //val distancia = parque_veterans.distanceToAsDouble(marcador.position) //calculate distance from one geoPoint to the other Geo point
         //Log.v("Distancia", "La distancia es ${distancia}")
@@ -67,7 +68,7 @@ fun MapaPokemones(controlador_gps: GPSControlador){
 
         mapa_view.overlays.add(marcador_ubicacion_actual)
         //mapa_view.overlays.add(marcador)
-        //mapa_view.overlays.add(marcador_2)
-        //mapa_view.overlays.add(marcador_3)
+        mapa_view.overlays.add(marcador_2)
+        mapa_view.overlays.add(marcador_3)
     }
 }
